29d1b4b3e5cb18523563614715efd35c3559ac22
tran quoc huy <tranquochuy15042003@gmail.com>
2025-07-08 16:45:12 +0700

    1

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@ echo "== STARTING PREFECT ORION SERVER =="
 prefect orion start --host 0.0.0.0 --port 4200 &
 ORION_PID=$!
 
-sleep 10  # Chờ server khởi động xong
+sleep 30  # Chờ server khởi động xong
 
 echo "== CREATING WORK POOL IF NOT EXISTS =="
 prefect work-pool inspect local-process-pool || \
 prefect work-pool create local-process-pool --type process
 
 echo "== STARTING PREFECT WORKER =="
-prefect worker start --pool local-process-pool --type process &
+prefect worker start --pool local-process-pool --type process 
 
 # Giữ container sống
 wait $ORION_PID
