0fb7f23e988b6ddb3a71d4a3a118060162b7369e
tran quoc huy <tranquochuy15042003@gmail.com>
2025-07-09 10:40:04 +0700

    deploy

--- start.sh
+++ start.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-set -e
+source .env
 
-# Load biến môi trường
-if [ -f .env ]; then
-  export $(cat .env | xargs)
-else
-  echo ".env file not found"
-  exit 1
-fi
-
-echo "== PREFECT CONFIG =="
+echo "== PREFECT UI/ API =="
 echo "PREFECT_API_URL=$PREFECT_API_URL"
 echo "PREFECT_UI_URL=$PREFECT_UI_URL"
 
+# Ép Prefect biết đúng URL public
 export PREFECT_API_URL=$PREFECT_API_URL
 export PREFECT_UI_URL=$PREFECT_UI_URL
 
-echo "== STARTING PREFECT SERVER (background) =="
+echo "== STARTING PREFECT SERVER =="
 prefect server start --host 0.0.0.0 --port 4200 &
 
 SERVER_PID=$!
+sleep 30
 
-# Chờ server khởi động
-sleep 60
+
+echo "== CREATE WORK POOL (if not exists) =="
+prefect work-pool create -t process local-process-pool || true
+
+echo "== STARTING PREFECT WORKER =="
+prefect worker start --pool local-process-pool --type process
+
+sleep 10
 
 echo "== SERVING FLOW =="
 python my_flows.py
