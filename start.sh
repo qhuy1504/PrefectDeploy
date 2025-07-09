846c232fb2b6685aabc10f19c4a1dbeb652426a0
tran quoc huy <tranquochuy15042003@gmail.com>
2025-07-09 09:06:17 +0700

    2

--- start.sh
+++ start.sh
@@ -5,12 +5,15 @@ export PREFECT_API_URL=https://prefect-production-c7b6.up.railway.app/api
 export PREFECT_UI_URL=https://prefect-production-c7b6.up.railway.app
 
 echo "== STARTING PREFECT SERVER =="
-prefect server start --host 0.0.0.0 --port 4200 --ui-url $PREFECT_UI_URL &
+# Khởi chạy prefect server
+prefect server start --host 0.0.0.0 --port 4200 &
 
 SERVER_PID=$!
 sleep 60
 
 echo "== STARTING PREFECT WORKER =="
+# Worker dùng API URL đã set bên trên
 prefect worker start --pool local-process-pool --type process
 
+# Sau khi worker tắt, kill server
 kill $SERVER_PID
